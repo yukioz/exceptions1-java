f72c060eb0622de402645063c0db71ee866e67b0
yukioz <yukio.link@gmail.com>
2020-08-06 16:56:20 -0300

    Criação da classe Exception e qualidade de software

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,85 +4,92 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
-	
-	//Construtores
-	
+
+	// Construtores
+
 	public Reservation() {
-		
+
 	}
+
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+
+		if (checkIn.before(now) || checkOut.before(now)) {
+
+			throw new DomainException("Reservation dates for update must be future dates");
+		}
+		if (!checkOut.after(checkIn)) {
+
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
-	
-	//Getters and Setters
-	
+
+	// Getters and Setters
+
 	public Integer getRoomNumber() {
 		return roomNumber;
 	}
+
 	public void setRoomNumber(Integer roomNumber) {
 		this.roomNumber = roomNumber;
 	}
-	
-	//Get dates
+
+	// Get dates
 	public Date getcheckIn() {
 		return checkIn;
 	}
+
 	public Date getcheckOut() {
 		return checkOut;
 	}
 
-	//Métodos
-	
+	// Métodos
+
 	public long duration() {
-		
-		//Converte tempo em data para milisegundos tipo long
+
+		// Converte tempo em data para milisegundos tipo long
 		long diff = checkOut.getTime() - checkIn.getTime();
-		//Converte tempo em milisegundos para dias tipo long
+		// Converte tempo em milisegundos para dias tipo long
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public String updatedates(Date checkIn, Date checkOut) {
-		
+
+	public void updatedates(Date checkIn, Date checkOut) {
+
 		Date now = new Date();
-		
-		if(checkIn.before(now) || checkOut.before(now)) {
-			
-			return "Error in reservation: Reservation dates for update must be future dates";
+
+		if (checkIn.before(now) || checkOut.before(now)) {
+
+			throw new DomainException("Reservation dates for update must be future dates");
 		}
-		if(!checkOut.after(checkIn)) {
-			
-			return "Error in reservation: Check-out date must be after check-in date";
+		if (!checkOut.after(checkIn)) {
+
+			throw new DomainException("Check-out date must be after check-in date");
 		}
-		
+
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		
-		return null;
 	}
-	
+
 	@Override
 	public String toString() {
-		
-		return "Room "
-				+ roomNumber
-				+ ", checkIn: "
-				+ sdf.format(checkIn)
-				+ ", checkOut: "
-				+ sdf.format(checkOut)
-				+ ", "
-				+ duration()
-				+ " night";
-		
+
+		return "Room " + roomNumber + ", checkIn: " + sdf.format(checkIn) + ", checkOut: " + sdf.format(checkOut) + ", "
+				+ duration() + " night";
+
 	}
-	
-	
+
 }
